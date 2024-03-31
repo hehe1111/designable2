b6d8ee27fad3329fcdb757e00ecc900263dd15ec
hehe1111 <2908749709@qq.com>
2024-03-31 19:25:10 +0800

    feat(formily/antd): 明确保存提示问题

--- formily/antd/playground/service/schema.ts
+++ formily/antd/playground/service/schema.ts
@@ -10,7 +10,7 @@ export const saveSchema = (designer: Engine) => {
     'formily-schema',
     JSON.stringify(transformToSchema(designer.getCurrentTree()))
   )
-  message.success('Save Success')
+  message.success('Save to LocalStorage Success')
 }
 
 export const loadInitialSchema = (designer: Engine) => {
